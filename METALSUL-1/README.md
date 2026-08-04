d9415d5ab7f1b8c5ac7a86f28545aad64b9ebe12
DOUGLAS DE SOUZA PAIXÃO <douglas_paixao@estudante.sesisenai.org.br>
2026-08-04 16:24:58 -0300

    feat: Adiciona algumas funcionalidades pequenas

--- METALSUL-1/README.md
+++
@@ -1,3 +0,0 @@
-# metalsul_industria
-
-# Classes
--- METALSUL-1/models/funcionario.py
+++
@@ -1,6 +0,0 @@
-class funcionario:
-    def __init__(self, nome, cpf, cargo, salario):
-        self.nome = nome
-        self.cpf = cpf
-        self.cargo = cargo
-        self.salario = salario
